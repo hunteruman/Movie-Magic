022821afce6fd8e0deedc729571997c280b9db89
hunteruman <hunteruman@gmail.com>
2021-12-09 10:45:11 -0500

    listcompare search implementation plus commented out code in main that im using to test my algorithms

--- Database.cpp
+++ Database.cpp
@@ -5,7 +5,7 @@ void Database::addEdge(string movie, string person, string role) {
     People[person].push_back(pair<string,string>(movie,role));
 }
 
-vector<string> Database::bfs(string person1, string person2) {
+vector<string> Database::dfs(string person1, string person2) {
     vector<string> out; // movie list
 
     // deciding shorter start list for optimisation purposes
@@ -22,7 +22,7 @@ vector<string> Database::bfs(string person1, string person2) {
         to = person2;
     }
 
-    // breadth-first search
+    // depth-first search
     string currMovie;
     for (int i = 0; i < People[from].size(); i++)
     {
@@ -44,6 +44,38 @@ vector<string> Database::bfs(string person1, string person2) {
 vector<string> Database::listCompare(string person1, string person2) {
     vector<string> out;
 
+    vector<string> p1; // person1's movies
+    vector<string> p2; // person2's movies
+
+    for (int i = 0; i < People[person1].size(); i++)
+        p1.push_back(People[person1][i].first);
+    for (int i = 0; i < People[person2].size(); i++)
+        p2.push_back(People[person2][i].first);
+
+    sort(p1.begin(),p1.end()); // nlogn time
+    sort(p2.begin(),p2.end()); // defines complexity for this search
+
+    int i = 0;
+    int j = 0;
+
+    // searching two sorted lists for commonalities
+    while((i < p1.size()) && (j < p2.size()))
+    {
+        if (p1[i] == p2[j])
+        {
+            out.push_back(p1[i]);
+            i++;
+            j++;
+        }
+        else if (p1[i] < p2[j])
+        {
+            i++;
+        }
+        else
+        {
+            j++;
+        }
+    }
 
     return out;
 }
