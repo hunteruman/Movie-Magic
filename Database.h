8f0e8533655838dba595cf1c9aab4263bc5b6d45
hunteruman <hunteruman@gmail.com>
2021-12-09 17:16:18 -0500

    alt structure implemented

--- Database.h
+++ Database.h
@@ -15,6 +15,8 @@ class Database {
     unordered_map<string, vector<pair<string, string>>> People;
 
     // Alt Adjacency List implementation of Map
+    unordered_map<string, unordered_map<string, vector<string>>> AltMovies;
+    unordered_map<string, unordered_map<string, vector<string>>> AltPeople;
 
 public:
 
@@ -25,5 +27,13 @@ public:
     vector<string> dfs(string person1, string person2);
     vector<string> sortedListCompare(string person1, string person2);
     vector<string> unsortedListCompare(string person1, string person2);
+
+    // alt map creation
+    void altAddEdge(string movie, string person, string role);
+
+    // alt search algorithms
+    vector<string> altDfs(string person1, string person2);
+    vector<string> altSortedListCompare(string person1, string person2);
+    vector<string> altUnsortedListCompare(string person1, string person2);
 
 };
